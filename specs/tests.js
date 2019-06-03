931223c30d7d2347c6ab35a8d9180cab428195c1
Julian Mourell <j.mourell@gmail.com>
2019-06-03 10:01:58 +0900

    Implement GuessThisNumber and Tests

--- specs/tests.js
+++ specs/tests.js
@@ -22,20 +22,62 @@ describe("gameGenerator", () => {
   });
 
   it("should have a reset method", () => {
+    const bound = 4;
+    const game = gameGenerator(bound);
     // How do you test for this?
-    expect(false).toBeTruthy();
+    expect(game.reset).toBeTruthy();
   });
 
-  it("create your own test", () => {
-    expect(false).toBeTruthy();
+  it("Expect game to reset number of guesses", () => {
+    const bound = 4;
+    const game = gameGenerator(bound);
+    const number = [];
+    for (let i = 0; i < bound; i++) {
+      if (game.guess(i)) {
+        number.push(i);
+      }
+    }
+    game.reset();
+    expect(game.numGuesses()===0).toBeTruthy();
+  });
+  it("Expect game to reset number of guesses", () => {
+    const bound = 4;
+    const game = gameGenerator(bound);
+    const number = [];
+    for (let i = 0; i < bound; i++) {
+      if (game.guess(i)) {
+        number.push(i);
+      }
+    }
+    expect(game.numGuesses()!==0).toBeTruthy();
+    game.reset();
+    expect(game.numGuesses()===0).toBeTruthy();
+  });
+  it("Expect number of guesses to equal one", () => {
+    const bound = 3;
+    const game = gameGenerator(bound);
+    game.guess(2)
+    expect(game.numGuesses()===1).toBeTruthy();
+    });
+
+  it("Expect giveUp to return number", () => { 
+    const bound = 4;
+    const game = gameGenerator(bound);
+    let number ;
+    for (let i = 0; i < bound; i++) {
+      if (game.guess(i)) {
+        number = i;
+      }
+    }
+    expect(game.giveUp()===number).toBeTruthy();
   });
 });
-
 describe("accountGenerator", () => {
   it("should be there", () => {
     expect(accountGenerator).toBeDefined();
     expect(typeof accountGenerator).toBe("function");
   });
+  
 
   it("should have some tests", () => {
     expect(false).toBeTruthy();
--- src/closures.js
+++ src/closures.js
@@ -8,7 +8,76 @@ function randomInteger(n) {
   return Math.floor(Math.random() * (n + 1));
 }
 
-function gameGenerator() {}
+function gameGenerator(upperLimit) {
+// returns an object
+let upper = upperLimit;
+//take a number input that provides the 'upper bound' (limit)
+// - [ ] keep track of how many guesses have been made
+  return {
+    // - [ ] generate a random number between 0 and the upper bound
+    number: randomInteger(upper) ,
+    numOfGuesses: 0,
+    // - [ ] have a method `reset` that resets the game (new winning number, reset guesses, same upper bound)
+  
+    reset: function () {
+      this.number=randomInteger(upper);
+      this.numOfGuesses=0;
+    },
+    // - [ ] have a method `giveUp` that returns the correct number and resets the game
+  
+    giveUp:function(){
+      let pastGameNumber=this.number;
+      this.reset();
+      return pastGameNumber;
+      
+    },
+    // - [ ] have a method `guess` that allows you to guess the number and returns true or false if the guess is right or wrong
+  
+    guess:function(num){
+      if (num > upper) {
+          return false;
+      } 
+      this.numOfGuesses++;
+      if (num===this.number){
+        return true;
+      } else {
+        return false;
+      }
+    },
+    // - [ ] have a method `numGuesses` that provides a way to see the number of guesses that have been made
+  
+    numGuesses:function() {
+      return this.numOfGuesses;
+    },
+  }
+}
+
+
+  // function randomInteger(n) {
+  //   return Math.floor(Math.random() * (n + 1));
+  // }
+
+  // const upperBound = 5;
+
+  // function guessThisNumber(n) {
+  //   if (n > upperBound) {
+  //     return {
+  //       message: `Wrong. Please try a number between 0 and ${upperBound}.`,
+  //       status: false
+  //     };
+  //   } else if (n === randomInteger(upperBound)) {
+  //     return {
+  //       message: "You win!",
+  //       status: true
+  //     };
+  //   }
+
+  //   return {
+  //     message: `Wrong. Please try a number between 0 and ${upperBound}.`,
+  //     status: false
+  //   };
+  // }
+
 
 function accountGenerator(initial) {
   let balance = initial;
